3139a9e7cab4f914d099fa20fa6a63d93f0ffb31
TylerBoyles19 <tylermboyles@outlook.com>
2020-07-27 10:46:27 -0500

    Created new project in FPJobBoard

---
+++ FPJobBoard.UI.EF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FPJobBoard.UI.EF.MvcApplication" Language="C#" %>
---
+++ FPJobBoard.UI.EF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FPJobBoard.UI.EF.Startup))]
+namespace FPJobBoard.UI.EF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
